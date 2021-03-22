eaf203b4503ea50729899b3c93c6369b3fd7dea1
Jeffrey Li <jeffreyli0211@gmail.com>
2021-03-21 23:57:55 -0400

    Second version of back-end implementation

---
+++ src/coaches/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from coaches.api.views import (
+    api_detail_coach_view,
+    ApiCoachListView
+)
+
+app_name = 'coaches'
+
+urlpatterns = [
+    path('list', ApiCoachListView.as_view(), name="list"),
+    path('<slug>', api_detail_coach_view, name='detail'),
+]
---
+++ src/readme.md
@@ -0,0 +1,5 @@
+# SETUP:
+        
+To run the project on the server:
+- go into the src/evolvu folder
+- run the following: py manage.py runserver
